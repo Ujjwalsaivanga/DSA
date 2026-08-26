9a911726d5fa7b7854820891907b7b084772d47d
ujjwalsaivanga <ujjwalsaivanga@gmail.com>
2026-08-26 22:25:09 +0530

    Time: 0 ms (100.00%) | Memory: 33.6 MB (66.12%) - LeetSync

---
+++ 4212-toggle-light-bulbs/toggle-light-bulbs.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> toggleLightBulbs(vector<int>& bulbs) {
+        vector<int>s(101);
+        vector<int>ans;
+        for(int i =0;i<101;i++){
+            s[i]=0;
+        }
+        for(int i=0;i<bulbs.size();i++){
+            if(s[bulbs[i]]==0){
+                s[bulbs[i]]=1;
+            }else{
+                s[bulbs[i]]=0;
+            }
+        }
+        for(int i=0;i<101;i++){
+            if(s[i]==1){
+                ans.push_back(i);
+            }
+        }
+return ans;
+    }
+};
